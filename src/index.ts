3c2d9be237290ec4c05a660c50c5c12a40ca40b0
Miguel Oller <maoa22@gmail.com>
2019-12-13 13:58:25 -0500

    Remove Helm from the reproduction

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
-const chart = new k8s.helm.v2.Chart("google", {
-  path: "./google",
-  transformations: [addImportForHelmChart()]
-});
-
-function addImportForHelmChart() {
-  return (obj: any, opts: pulumi.CustomResourceOptions): void => {
-    if (obj != null && obj.metadata != null && obj.metadata.name) {
-      opts.import = obj.metadata.namespace
-        ? `${obj.metadata.namespace}/${obj.metadata.name}`
-        : obj.metadata.name;
-    }
-  };
-}
+const service = new k8s.core.v1.Service(
+  "google",
+  {
+    metadata: { name: "google", annotations: {} },
+    spec: { type: "ExternalName", externalName: "www.google.com" }
+  },
+  { import: "default/google" }
+);
